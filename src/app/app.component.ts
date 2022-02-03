57959b0e62860ee334b0412ceaa7595e400fd27a
ronaldoPro <51919341+Ronaldo1999@users.noreply.github.com>
2022-02-03 14:58:36 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  name: string = '';
   title = 'coursang';
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    const double = (x: number) => x * 2;
+    console.log(double(3));
+    console.log(this.addAndDouble(3, 2));
+    ;
+  }
+
+  addAndDouble = (x: any, y: any) => {
+    const sum = x + y;
+    return sum * 2;
+  }
+
 }
